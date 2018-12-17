846f8c39801678cadb762cd5e007c5849ec53ace
Kerndog73 <kerndog73@gmail.com>
2018-12-17 10:47:54 +1030

    Attempt to use new pass builder
    I cannot find a single example for using the llvm::PassBuilder class. No examples at all! Maybe I'm looking in the wrong places. This is just frustrating. The docs are outdated. The official tutorial uses the old API.
    
    FFS

--- src/optimize module.cpp
+++ src/optimize module.cpp
@@ -11,9 +11,11 @@
 #include "unreachable.hpp"
 #include <llvm/IR/Verifier.h>
 #include <llvm/Transforms/IPO.h>
+#include <llvm/Passes/PassBuilder.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Target/TargetMachine.h>
 #include <llvm/Transforms/IPO/GlobalDCE.h>
+#include <llvm/Transforms/Utils/CtorUtils.h>
 #include <llvm/Analysis/TargetLibraryInfo.h>
 #include <llvm/Analysis/TargetTransformInfo.h>
 #include <llvm/Transforms/IPO/PassManagerBuilder.h>
@@ -32,6 +34,7 @@ void addOptPasses(
   builder.LoopVectorize = true;
   builder.SLPVectorize = true;
   builder.NewGVN = true;
+  builder.PrepareForLTO = true;
   machine->adjustPassManager(builder);
   
   builder.populateFunctionPassManager(fnPasses);
@@ -40,9 +43,20 @@ void addOptPasses(
 
 void addLinkPasses(llvm::legacy::PassManagerBase &passes) {
   llvm::PassManagerBuilder builder;
-  builder.VerifyInput = true;
+  builder.PrepareForLTO = true;
   builder.Inliner = llvm::createFunctionInliningPass(3, 0, false);
   builder.populateLTOPassManager(passes);
+}
+
+bool shouldRemoveCtor(llvm::Function *ctor) {
+  if (ctor->size() > 1) {
+    return false;
+  }
+  llvm::BasicBlock *entry = &ctor->getEntryBlock();
+  if (entry->size() > 1) {
+    return false;
+  }
+  return entry->front().isTerminator();
 }
 
 }
@@ -59,6 +73,28 @@ std::unique_ptr<Module> getLazyIRFileModule(StringRef, SMDiagnostic &, LLVMConte
 void stela::optimizeModule(llvm::TargetMachine *machine, llvm::Module *module) {
   module->setTargetTriple(machine->getTargetTriple().str());
   module->setDataLayout(machine->createDataLayout());
+  
+  /*
+  
+  // need to link /usr/local/opt/llvm/lib/libLLVMPasses.a
+  // crashes in ModuleAnalysisManager destructor
+  
+  llvm::ModuleAnalysisManager MAM;
+  llvm::FunctionAnalysisManager FAM;
+  llvm::LoopAnalysisManager LAM;
+  llvm::CGSCCAnalysisManager CGAM;
+  llvm::PassBuilder PB;
+  
+  PB.registerModuleAnalyses(MAM);
+  PB.registerFunctionAnalyses(FAM);
+  PB.registerLoopAnalyses(LAM);
+  PB.registerCGSCCAnalyses(CGAM);
+  PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
+  
+  auto MPM = PB.buildModuleOptimizationPipeline(llvm::PassBuilder::O3);
+  MPM.run(*module, MAM);
+  
+  */
 
   llvm::legacy::PassManager passes;
   passes.add(new llvm::TargetLibraryInfoWrapperPass(machine->getTargetTriple()));
@@ -76,12 +112,8 @@ void stela::optimizeModule(llvm::TargetMachine *machine, llvm::Module *module) {
   }
   fnPasses.doFinalization();
 
-  llvm::GlobalDCEPass dce;
-  llvm::ModuleAnalysisManager modMan;
-  dce.run(*module, modMan);
-
   passes.add(llvm::createVerifierPass());
   passes.run(*module);
   
-  dce.run(*module, modMan);
+  //llvm::optimizeGlobalCtorsList(*module, &shouldRemoveCtor);
 }
