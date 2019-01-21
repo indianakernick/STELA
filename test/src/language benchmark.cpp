d3024cf0db021c3852f586816cf9f53232393002
Kerndog73 <kerndog73@gmail.com>
2019-01-21 17:48:47 +1030

    Write another benchmark
    Project euler problems might make good benchmarks. I might write a physics simulation as well because a large program can be used to benchmark the compiler as well as the language.
    
    I need to write big programs in Stela.

--- test/src/language benchmark.cpp
+++ test/src/language benchmark.cpp
@@ -6,6 +6,8 @@
 //  Copyright © 2019 Indi Kernick. All rights reserved.
 //
 
+#include <iostream>
+#include <unordered_set>
 #include <STELA/llvm.hpp>
 #include <STELA/binding.hpp>
 #include <benchmark/benchmark.h>
@@ -18,15 +20,33 @@ using namespace stela;
 
 namespace {
 
-llvm::ExecutionEngine *generate(const stela::Symbols &syms, LogSink &log) {
-  return stela::generateCode(stela::generateIR(syms, log), log);
+llvm::ExecutionEngine *generate(const stela::Symbols &syms, LogSink &log, const char *id) {
+  static std::unordered_set<const char *> printed;
+  const bool print = printed.insert(id).second;
+  std::unique_ptr<llvm::Module> module = stela::generateIR(syms, log);
+  llvm::Module *modulePtr = module.get();
+  std::string str;
+  llvm::raw_string_ostream strStream(str);
+  if (print) {
+    strStream << *modulePtr;
+    strStream.flush();
+    std::cerr << "Generated IR\n" << str;
+    str.clear();
+  }
+  llvm::ExecutionEngine *engine = stela::generateCode(std::move(module), log);
+  if (print) {
+    strStream << *modulePtr;
+    strStream.flush();
+    std::cerr << "Optimized IR\n" << str;
+  }
+  return engine;
 }
 
 llvm::ExecutionEngine *generate(const std::string_view source, LogSink &log) {
   stela::AST ast = stela::createAST(source, log);
   stela::Symbols syms = stela::initModules(log);
   stela::compileModule(syms, ast, log);
-  return generate(syms, log);
+  return generate(syms, log, source.data());
 }
 
 template <typename Fun, bool Member = false>
@@ -66,7 +86,7 @@ void squares(::benchmark::State &state) {
     ::benchmark::DoNotOptimize(array);
   }
 }
-BENCHMARK(squares)->Range(512, 1<<16)->Unit(benchmark::kMicrosecond);
+BENCHMARK(squares)->Range(512, 65536)->Unit(benchmark::kMicrosecond);
 
 void squaresNoReserve(::benchmark::State &state) {
   ensureLLVM();
@@ -87,6 +107,45 @@ void squaresNoReserve(::benchmark::State &state) {
     ::benchmark::DoNotOptimize(array);
   }
 }
-BENCHMARK(squaresNoReserve)->Range(512, 1<<16)->Unit(benchmark::kMicrosecond);
+BENCHMARK(squaresNoReserve)->Range(512, 65536)->Unit(benchmark::kMicrosecond);
+
+void euler1(::benchmark::State &state) {
+  ensureLLVM();
+  
+  auto *engine = generate(R"(
+    extern func calc(n: uint) {
+      var sum = 0u;
+      for (i := 0u; i != n; i++) {
+        if (i % 3u == 0u || i % 5u == 0u) {
+          sum += i;
+        }
+      }
+      return sum;
+    }
+  )", log());
+  auto calc = getFunc<Uint(Uint)>(engine, "calc");
+  
+  for (auto _ : state) {
+    ::benchmark::DoNotOptimize(calc(state.range()));
+  }
+}
+BENCHMARK(euler1)->Arg(1000)->Arg(10000)->Unit(benchmark::kMicrosecond);
+
+Uint calcEuler1(const Uint n) {
+  Uint sum = 0;
+  for (Uint i = 0; i != n; ++i) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+void euler1_cpp(::benchmark::State &state) {
+  for (auto _ : state) {
+    ::benchmark::DoNotOptimize(calcEuler1(static_cast<Uint>(state.range())));
+  }
+}
+BENCHMARK(euler1_cpp)->Arg(1000)->Arg(10000)->Unit(benchmark::kMicrosecond);
 
 }
